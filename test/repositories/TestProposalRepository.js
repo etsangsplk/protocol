9cf6d3ed60ece1089f41e857f31ce54be424eb8e
decanus <dean.eigenmann@icloud.com>
2017-07-29 12:52:28 +0200

    Added first test, with mock

---
+++ test/repositories/TestProposalRepository.js
@@ -0,0 +1,30 @@
+const ProposalRepository = artifacts.require('repositories/ProposalRepository.sol');
+var ExecutorMock = artifacts.require("mocks/ExecutorMock.sol");
+var ProposalFactory = artifacts.require("factories/ProposalFactoryInterface.sol");
+
+let repository;
+
+contract('ProposalRepository', function (accounts) {
+
+        let shouldntFail = function (err) {
+            assert.isFalse(!!err);
+        };
+
+        beforeEach(async () => {
+            repository = await ProposalRepository.new();
+        });
+
+        it('verifies that proposal can be added to repository', async () => {
+
+            let executor = await ExecutorMock.new();
+            let factory = await ProposalFactory.new();
+
+            let result = await repository.add(
+                "foo",
+                executor.address,
+                factory.address
+            );
+
+            assert.equal(result.logs[0].event, 'ProposalAdded', 'proposal was not added');
+        });
+});
