6616e56cbf732aa6c2baae60101f51eb2df23234
decanus <dean.eigenmann@icloud.com>
2017-07-31 22:12:33 +0200

    added first example of rights

---
+++ contracts/voting/WhitelistRights.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.11;
+
+import "./VotingRights.sol";
+
+contract WhitelistRights is VotingRights {
+
+    mapping (address => bool) public whitelisted;
+
+    function canVote(address voter) constant returns (bool) {
+        return whitelisted[voter];
+    }
+
+    function canPropose(address proposer) constant returns (bool) {
+        return whitelisted[voter];
+    }
+}
