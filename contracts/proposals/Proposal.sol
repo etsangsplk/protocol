2a640075c83a3c0cfe2b954d1badced9ebb6abcf
decanus <dean.eigenmann@icloud.com>
2017-08-08 21:19:17 +0200

    Removed comment

--- contracts/proposals/Proposal.sol
+++ contracts/proposals/Proposal.sol
@@ -46,33 +46,4 @@ contract Proposal {
 
         return false;
     }
-
-    // @todo move into quorum contract
-    /*function didPass() returns (bool) {
-        uint yes = 0;
-        uint no = 0;
-
-        for (uint i = 0; i < votes.length; i++) {
-            Vote memory vote = votes[i];
-            uint voteBalance = token.balanceOf(vote.voter);
-
-            if (vote.inFavour) {
-                yes += voteBalance;
-                continue;
-            }
-
-            no += voteBalance;
-        }
-
-        return yes > no;
-    }
-
-    function getVoterQuorum() returns (uint) {
-        uint sum = 0;
-        for (uint i = 0; i < votes.length; i++) {
-            sum += token.balanceOf(votes[i].voter);
-        }
-
-        return (sum / token.totalSupply()) * 100;
-    }*/
 }
