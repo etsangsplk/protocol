49985f5757ba1f8ef3d11f90fb6ecad9e0aef4c3
decanus <dean.eigenmann@icloud.com>
2017-08-09 21:36:15 +0200

    Quick vault interface

---
+++ contracts/vault/VaultInterface.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.11;
+
+contract VaultInterface {
+
+    function transfer(address token, address to, uint256 amount) external;
+    function transferEther(address to, uint256 amount) external;
+
+}
