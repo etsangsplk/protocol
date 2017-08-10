a8dfb4311e0d95ebab1808e80915319c348978b9
decanus <dean.eigenmann@icloud.com>
2017-08-10 21:15:00 +0200

    Added method to interfac

--- contracts/vault/VaultInterface.sol
+++ contracts/vault/VaultInterface.sol
@@ -4,5 +4,6 @@ contract VaultInterface {
 
     function transfer(address token, address to, uint256 amount) external;
     function transferEther(address to, uint256 amount) external;
+    function balanceOf(address _token) public constant returns (uint);
 
 }
