14b4a39ac2d71a227bc2c8a48943c1ad3efebeb8
decanus <dean.eigenmann@icloud.com>
2017-08-10 21:15:29 +0200

    Updated method signature

--- contracts/vault/VaultInterface.sol
+++ contracts/vault/VaultInterface.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.11;
 
 contract VaultInterface {
 
-    function transfer(address token, address to, uint256 amount) external;
+    function transfer(address _token, address to, uint256 amount) external;
     function transferEther(address to, uint256 amount) external;
     function balanceOf(address _token) public constant returns (uint);
 
