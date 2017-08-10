0ab618ad6bd64264b58c65e93faeb736eb28e5b6
decanus <dean.eigenmann@icloud.com>
2017-08-10 20:38:51 +0200

    Added Vault contract

---
+++ contracts/vault/Vault.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.11;
+
+import "./VaultInterface.sol";
+import "./ERC20.sol";
+
+contract Vault is VaultInterface {
+
+    function transfer(address _token, address to, uint256 amount) external {
+        ERC20 token = ERC20(_token);
+        require(token.balanceOf(token) >= amount);
+        token.transfer(to, amount);
+    }
+
+    function transferEther(address to, uint256 amount) external {
+        require(this.balance >= amount);
+        to.transfer(amount);
+    }
+}
