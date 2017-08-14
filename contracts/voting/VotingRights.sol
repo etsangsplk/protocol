0da46292387a292d76be42f83b3aaf9bc132abae
decanus <dean.eigenmann@icloud.com>
2017-08-14 19:38:28 +0200

    Added 2 new functions

--- contracts/voting/VotingRights.sol
+++ contracts/voting/VotingRights.sol
@@ -4,5 +4,7 @@ contract VotingRights {
 
     function canVote(address voter) constant returns (bool);
     function canPropose(address proposer) constant returns (bool);
+    function canApprove(address approver) constant returns (bool);
+    function requiresApproval(/* will require proposal id */) constant returns (bool);
 
 }
