983df08b6a5e64fcfc345518f08ed4b86567c2c6
decanus <dean.eigenmann@icloud.com>
2017-08-19 00:33:48 +0200

    added create

--- contracts/voting/Voting.sol
+++ contracts/voting/Voting.sol
@@ -18,6 +18,16 @@ contract Voting is ownable {
 
     ProposalData[] proposals;
 
+    function create(address creator, address proposal) external onlyOwner returns (uint) {
+        ProposalData storage proposal = proposals[proposals.length];
+
+        proposal.creator = creator;
+        proposal.proposal = proposal;
+        proposal.approved = false;
+
+        return proposals.length;
+    }
+
     function vote(uint id, address voter, uint8 choice) external onlyOwner {
         require(!proposals[id].voted[voter]);
         require(isValidChoice(id, choice));
