dee415b9febfe1adfed87435360e32c1dba2d1bc
decanus <dean.eigenmann@icloud.com>
2017-08-19 10:39:10 +0200

    minor update

--- contracts/proposals/Proposal.sol
+++ contracts/proposals/Proposal.sol
@@ -4,17 +4,17 @@ import "../ownership/ownable.sol";
 
 contract Proposal is ownable {
 
-    uint8[] public validChoices;
+    uint8[] public choices;
 
     function Proposal(uint8[] _choices) {
         for (uint i = 0; i < _choices.length; i++) {
-            validChoices.push(_choices[i]);
+            choices.push(_choices[i]);
         }
     }
 
     function isValidChoice(uint8 _choice) constant returns (bool) {
-        for (uint i = 0; i < validChoices.length; i++) {
-            if (validChoices[i] == _choice) {
+        for (uint i = 0; i < choices.length; i++) {
+            if (choices[i] == _choice) {
                 return true;
             }
         }
