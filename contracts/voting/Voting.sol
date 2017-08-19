e9bd3d498baddfb9df4e9889ff69a235d389000b
decanus <dean.eigenmann@icloud.com>
2017-08-19 12:37:10 +0200

    added documentation comments

--- contracts/voting/Voting.sol
+++ contracts/voting/Voting.sol
@@ -18,6 +18,10 @@ contract Voting is ownable {
 
     ProposalData[] proposals;
 
+    /// @dev Creates a new proposal and stores it.
+    /// @param creator Address of the proposal creator.
+    /// @param proposal Address of the proposal contract.
+    /// @return id of the created proposal.
     function create(address creator, address proposal) external onlyOwner returns (uint) {
         uint id = proposals.length;
         proposals.length++;
@@ -30,6 +34,10 @@ contract Voting is ownable {
         return id;
     }
 
+    /// @dev Votes on a proposal.
+    /// @param id Id of the proposal.
+    /// @param voter Address of the voter.
+    /// @param choice Users selected voting choice.
     function vote(uint id, address voter, uint8 choice) external onlyOwner {
         require(!proposals[id].voted[voter]);
         require(isValidChoice(id, choice));
@@ -40,14 +48,23 @@ contract Voting is ownable {
         proposal.voted[voter] = true;
     }
 
+    /// @dev Approves a proposal.
+    /// @param id Id of the proposal.
     function approve(uint id) external onlyOwner {
         proposals[id].approved = true;
     }
 
+    /// @dev Checks if a proposal has been approved.
+    /// @param id Id of the proposal.
+    /// @return bool if proposal is approved.
     function isApproved(uint id) constant returns (bool) {
         return proposals[id].approved;
     }
 
+    /// @dev Checks if a choice is valid
+    /// @param id Id of the proposal.
+    /// @param choice Users selected voting choice.
+    /// @return bool if choice is valid.
     function isValidChoice(uint id, uint8 choice) constant returns (bool) {
         return Proposal(proposals[id].proposal).isValidChoice(choice);
     }
