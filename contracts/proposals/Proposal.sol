455b8527fd312de04c0da938a6b2e37919623fd6
decanus <dean.eigenmann@icloud.com>
2017-08-20 14:11:11 +0200

    Removed constructor

--- contracts/proposals/Proposal.sol
+++ contracts/proposals/Proposal.sol
@@ -6,12 +6,6 @@ contract Proposal is ownable {
 
     uint8[] public choices;
 
-    function Proposal(uint8[] _choices) {
-        for (uint i = 0; i < _choices.length; i++) {
-            choices.push(_choices[i]);
-        }
-    }
-
     function isValidChoice(uint8 _choice) constant returns (bool) {
         for (uint i = 0; i < choices.length; i++) {
             if (choices[i] == _choice) {
