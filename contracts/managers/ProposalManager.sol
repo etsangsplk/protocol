ca01e721a50863185a55118fc41f9715d3b5c4da
decanus <dean.eigenmann@icloud.com>
2017-08-25 22:23:24 +0200

    Fix indentation

--- contracts/managers/ProposalManager.sol
+++ contracts/managers/ProposalManager.sol
@@ -4,41 +4,32 @@ import "../ownership/ownable.sol";
 
 contract ProposalManager is ownable {
 
-        struct ProposalData {
-            bool approved;
+    struct ProposalData {
+        bool approved;
 
-            address creator;
-            address proposal;
+        address creator;
+        address proposal;
 
-            address[] voters;
-            mapping (address => bool) voted;
-            mapping (address => uint8) choices;
-        }
+        address[] voters;
+        mapping (address => bool) voted;
+        mapping (address => uint8) choices;
+    }
 
-        ProposalData[] proposals;
+    ProposalData[] proposals;
 
-        /// @dev Adds a new proposal.
-        /// @param creator Address of the proposal creator.
-        /// @param proposal Address of the proposal contract.
-        /// @return id of the proposal.
-        function add(address creator, address proposal) external onlyOwner returns (uint) {
-            uint id = proposals.length;
-            proposals.length++;
+    /// @dev Adds a new proposal.
+    /// @param creator Address of the proposal creator.
+    /// @param proposal Address of the proposal contract.
+    /// @return id of the proposal.
+    function add(address creator, address proposal) external onlyOwner returns (uint) {
+        uint id = proposals.length;
+        proposals.length++;
 
-            ProposalData storage data = proposals[id];
-            data.creator = creator;
-            data.proposal = proposal;
-            data.approved = false;
+        ProposalData storage data = proposals[id];
+        data.creator = creator;
+        data.proposal = proposal;
+        data.approved = false;
 
-            return id;
-        }
-
-        function get() {
-
-        }
-
-        function remove() {
-
-        }
-
+        return id;
+    }
 }
