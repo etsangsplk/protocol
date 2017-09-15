7069fa6e6c6f1938ae1817da438139a971db2da9
decanus <dean.eigenmann@icloud.com>
2017-09-16 00:14:39 +0200

    ProposalManager test

---
+++ test/managers/TestProposalManager.js
@@ -0,0 +1,24 @@
+const MyProposalManager = artifacts.require('managers/ProposalManager.sol');
+
+contract('ProposalManager', function (accounts) {
+
+    let proposalManager;
+
+    beforeEach(async () => {
+        proposalManager = await MyProposalManager.new();
+    });
+
+    it('should allow owner to add a proposal', async () => {
+       await proposalManager.add(accounts[1], accounts[3]);
+       assert.equal(accounts[3], await proposalManager.getProposal.call(0), 'proposal address did not match expected value');
+    });
+
+
+    it('should allow owner to approve a proposal', async () => {
+       await proposalManager.add(accounts[1], accounts[3]);
+       assert.equal(false, await proposalManager.isApproved.call(0), 'proposal was not expected to be approved');
+       await proposalManager.approve(0);
+       assert.equal(true, await proposalManager.isApproved.call(0), 'proposal was not approved');
+    });
+
+});
