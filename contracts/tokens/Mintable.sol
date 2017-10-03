3fe9c8a5b982773e99a8c5b9c8a1a20c44185185
decanus <dean.eigenmann@icloud.com>
2017-10-03 18:16:49 +0200

    Removed Mint & Burn tokens

--- contracts/tokens/Mintable.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.15;
-
-import "../ownership/ownable.sol";
-
-contract Mintable is ownable {
-
-    event Mint(address indexed to, uint value);
-    function mint(address _to, uint _amount) public onlyOwner returns (bool);
-}
