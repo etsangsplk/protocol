c5cce66da19e59ad6f047ee18027f529003afaa6
Dean Eigenmann <dean@tokenate.io>
2017-11-01 10:28:46 +0100

    enhancement/gas-reporter (#118)
    * added gas reporter
    
    * added ethgas config
    
    * Added option
    
    * updated

--- truffle.js
+++ truffle.js
@@ -1,3 +1,13 @@
-module.exports = {
-    networks: { }
+let options = {
+    networks: { },
+    mocha: {
+        reporter: 'eth-gas-reporter'
+    }
 };
+
+let reporterArg = process.argv.indexOf('--report');
+if (reporterArg === -1) {
+    delete  options['mocha'];
+}
+
+module.exports = options;
