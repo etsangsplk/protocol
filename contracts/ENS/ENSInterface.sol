b3c3746d36bdfa4210ddada04862401062cb9611
Dean Eigenmann <dean@tokenate.io>
2017-11-08 14:27:50 +0100

    enhancement/module-proxy-ens (#122)

---
+++ contracts/ENS/ENSInterface.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.4.18;
+
+interface ENSInterface {
+    function owner(bytes32 node) public view returns (address);
+    function resolver(bytes32 node) public view returns (address);
+    function ttl(bytes32 node) public view returns (uint64);
+    function setOwner(bytes32 node, address owner) public;
+    function setSubnodeOwner(bytes32 node, bytes32 label, address owner) public;
+    function setResolver(bytes32 node, address resolver) public;
+    function setTTL(bytes32 node, uint64 ttl) public;
+}
---
+++ test/mocks/ENSResolverMock.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.4.18;
+
+contract ENSResolverMock {
+
+    mapping (bytes32 => address) resolvers;
+
+    function setResolver(bytes32 node, address resolver) public {
+        resolvers[node] = resolver;
+    }
+
+    function resolver(bytes32 node) public view returns (address) {
+        return resolvers[node];
+    }
+}
