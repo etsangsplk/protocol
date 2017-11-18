503b27f169aa326c8907caddc0eff397a4eb643f
decanus <dean.eigenmann@icloud.com>
2017-11-18 15:32:25 +0100

    added a todo

--- contracts/Voting/VotingRightsInterface.sol
+++ contracts/Voting/VotingRightsInterface.sol
@@ -6,6 +6,8 @@ interface VotingRightsInterface {
 
     function canVote(address voter, ProposalInterface proposal) public view returns (bool);
     function canPropose(address proposer) public view returns (bool);
+
+    // @todo consider do we need these functions here
     function canApprove(address approver) public view returns (bool);
     function requiresApproval(uint proposal) public view returns (bool);
 
